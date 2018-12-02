f7313c03d185f90a8db3d1143ca20f765b497762
azharmayo3a <30964680+azharmayo3a@users.noreply.github.com>
2018-12-02 02:49:26 +0100

    [Add]
    Classess

---
+++ Classes/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2018
+{
+    public class Student
+    {
+        public string matNo { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string group { get; set; }
+        public string city { get; set; }
+        public string plzcode { get; set; }
+        public DateTime birthdate { get; set; }
+        public string gender { get; set; }
+        public string languages { get; set; }
+        public string hobies { get; set; }
+        public string eMail { get; set; }
+        public string taskACS { get; internal set; }
+    }
+}
